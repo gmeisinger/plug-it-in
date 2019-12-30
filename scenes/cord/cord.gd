829b9b8af286d98b0156590face42eb312ce12a4
gmeisinger <gmeisinger@gmail.com>
2019-12-30 10:46:23 -0500

    patch applied, rope fixed

--- scenes/cord/cord.gd
+++ scenes/cord/cord.gd
@@ -1,49 +1,35 @@
 extends Node2D
 
-var SOFTNESS = 0.1
-var BIAS = 0.9
-export var length : int
+export var LENGTH : int
+export var SOFTNESS = 0.1
+export var BIAS = 0.9
 
-#export(String, FILE, "*.tscn") var segment_scene
 var segment = preload("res://scenes/cord/cord_segment.tscn")
-var joint = preload("res://scenes/cord/cord_joint.tscn")
-
-var joints = []
 
 func _ready():
-	#build_cord()
-	pass
-	
+	build_cord()
+
+func build_cord():
+	var parent = $anchor
+	for i in range(LENGTH):
+		var child = add_segment(parent)
+		add_joint(parent, child)
+		parent = child
+		# hide the last segment - looks better. Comment out to see difference
+		if i == LENGTH-1:
+			child.get_node('Sprite').hide()
+
 func add_segment(parent):
 	var new_segment = segment.instance()
-	new_segment.position = parent.get_end()
+	new_segment.position = parent.position + parent.get_end()
 	add_child(new_segment)
 	return new_segment
 
 func add_joint(parent, child):
-	var new_joint = joint.instance()
+	var new_joint = PinJoint2D.new()
 	new_joint.position = parent.get_end()
 	new_joint.node_a = parent.get_path()
 	new_joint.node_b = child.get_path()
+	new_joint.softness = SOFTNESS
+	new_joint.bias = BIAS
 	parent.add_child(new_joint)
-	joints.append(new_joint)
-
-func build_cord():
-	joints = []
-	var parent = $anchor
-	for i in range(length):
-		var child = add_segment(parent)
-		add_joint(parent, child)
-		parent = child
-
-func resolve_cord(delta):
-	for joint in joints:
-#		joint.resolve(delta)
-		pass
-
-func _physics_process(delta):
-	resolve_cord(delta)
-
-
-func _on_Timer_timeout():
-	build_cord()
