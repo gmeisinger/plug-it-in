5fa0fb82414a7558d153247b77b54210865ffb39
jayaarrgh <jrrdmb@gmail.com>
2020-01-03 20:57:02 -0600

    a pause menu

--- scenes/UI/PauseMenu.gd
+++ scenes/UI/PauseMenu.gd
@@ -1,13 +1,25 @@
 extends Control
 
-func _ready():
-	pass
+# Exists only in levels and world map
+
+const TITLE_SCREEN = "res://scenes/ui/Title.tscn"
+const WORLD_MAP = "res://scenes/map/WorldMap.tscn"
+
+func check_world_map():
+	if get_node('/root/WorldMap'):
+		$"VBoxContainer/World Map".hide()
+	else:
+		$"VBoxContainer/World Map".show()
 
 func leave_pause_menu():
 	$AnimationPlayer.play_backwards("Pop")
-	# queue_free() # ?
-	pass
+	yield($AnimationPlayer, "animation_finished")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Quit_pressed():
+	leave_pause_menu()
+	SceneLoader.load_scene(TITLE_SCREEN)
+	self.queue_free()
+
+func _on_World_Map_pressed():
+	leave_pause_menu()
+	SceneLoader.load_scene(WORLD_MAP)
--- scenes/map/LevelPortal.gd
+++ scenes/map/LevelPortal.gd
@@ -3,7 +3,7 @@ extends Node2D
 # LevelPortal is an area collider for the LevelDialog to start a level
 
 # LevelPortal depends on LevelDialog in WorldMap
-onready var level_dialog = get_node("../../LevelDialog")
+onready var level_dialog = get_node("/root/WorldMap/LevelDialog")
 
 # set in inspector per LevelPortal.tscn instance
 export (String) var level_name # for display/ui purposes only
