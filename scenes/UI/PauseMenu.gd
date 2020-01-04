6d88a3f4192ca8f1a5eecbb13b72ffd855f81809
jayaarrgh <jrrdmb@gmail.com>
2020-01-03 22:40:12 -0600

    pause menu implemented

--- scenes/UI/PauseMenu.gd
+++ scenes/UI/PauseMenu.gd
@@ -1,25 +1,46 @@
 extends Control
 
-# Exists only in levels and world map
+# quit from anywhere - handles escape key for now
 
-const TITLE_SCREEN = "res://scenes/ui/Title.tscn"
-const WORLD_MAP = "res://scenes/map/WorldMap.tscn"
+const TITLE_SCREEN = 'res://scenes/UI/Title.tscn'
+const WORLD_MAP = 'res://scenes/map/WorldMap.tscn'
 
-func check_world_map():
-	if get_node('/root/WorldMap'):
-		$"VBoxContainer/World Map".hide()
-	else:
-		$"VBoxContainer/World Map".show()
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_ESCAPE:
+			if visible:
+				leave_pause_menu()
+			elif has_node('/root/Title'):
+				get_tree().quit()
+			else: # in levels and world map
+				enter_pause_menu()
+
+func enter_pause_menu():
+	_check_world_map()
+	_ensure_on_top()
+	$AnimationPlayer.play('Pop')
 
 func leave_pause_menu():
-	$AnimationPlayer.play_backwards("Pop")
-	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play_backwards('Pop')
 
-func _on_Quit_pressed():
+func _ensure_on_top():
+	get_tree().get_root().move_child(self, get_tree().get_root().get_child_count())
+
+func _check_world_map():
+	if has_node('/root/WorldMap'):
+		$'Panel/VBoxContainer/WorldMapButton'.hide()
+	else:
+		$'Panel/VBoxContainer/WorldMapButton'.show()
+
+func _on_Resume_pressed():
 	leave_pause_menu()
-	SceneLoader.load_scene(TITLE_SCREEN)
-	self.queue_free()
 
 func _on_World_Map_pressed():
 	leave_pause_menu()
+	yield($AnimationPlayer, 'animation_finished')
 	SceneLoader.load_scene(WORLD_MAP)
+
+func _on_Quit_pressed():
+	leave_pause_menu()
+	yield($AnimationPlayer, 'animation_finished')
+	SceneLoader.load_scene(TITLE_SCREEN)
--- scenes/autoloads/Globals.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-# meant to run privately here -- not to be called elsewhere
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_ESCAPE:
-			get_tree().quit()
